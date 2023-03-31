3dc72f693d6cfe15f5a10ee9d6bb8eda60d0e2f5
Zhao vistart <i@vistart.me>
2023-04-01 01:07:10 +0800

    add redis component.

---
+++ component/redis.go
@@ -0,0 +1,31 @@
+package component
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisServerStatus struct {
+	Valid   bool   `json:"valid"`
+	Message string `json:"message"`
+}
+
+func GetRedisServerStatus(ctx context.Context, clients *[]*redis.Client) map[uint8]RedisServerStatus {
+	result := make(map[uint8]RedisServerStatus)
+	for i, c := range *clients {
+		status := RedisServerStatus{
+			Valid: false,
+		}
+		poolStats := c.PoolStats()
+		if _, err := c.Ping(ctx).Result(); err != nil {
+			status.Valid = false
+			status.Message = err.Error()
+		} else {
+			status.Valid = true
+			status.Message = fmt.Sprintf("命中:%d, 未命中:%d, 超时:%d, 总连接:%d, 空闲连接:%d, 失效连接:%d.", poolStats.Hits, poolStats.Misses, poolStats.Timeouts, poolStats.TotalConns, poolStats.IdleConns, poolStats.StaleConns)
+		}
+		result[uint8(i)] = status
+	}
+	return result
+}
