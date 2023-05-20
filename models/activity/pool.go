f5047bd1739c85c51b65d63cf27c3a4239177db7
Zhao vistart <i@vistart.me>
2023-05-20 20:33:55 +0800

    完善 Activity Pool。

--- models/activity/pool.go
+++ models/activity/pool.go
@@ -6,27 +6,49 @@ import (
 	"time"
 )
 
+// Pool defines an activity pool.
+// Please do not access any member directly, but use related methods unless you know the consequences of doing so.
 type Pool struct {
-	activities            map[uint64]*Activity
-	activitiesRWMutex     sync.RWMutex
-	activitiesLimit       uint32
+	// activities stores all the activities.
+	// The key of the map is the activity ID.
+	activities map[uint64]*Activity
+	// activitiesRWMutex describes the access mutex for activities.
+	activitiesRWMutex sync.RWMutex
+	// activitiesLimit defines the upper limit of the capacity.
+	activitiesLimit uint32
+	// activitiesIDGenerator defines the function handle that generates the activity ID.
+	// After the Pool is initialized, you can also modify this member at any time.
+	// If not specified, use NewActivityID()
 	activitiesIDGenerator func() uint64
 }
 
+// NewActivityPool initializes an activity pool.
 func NewActivityPool(limit uint32, IDGenerator func() uint64) *Pool {
-	pool := Pool{activities: map[uint64]*Activity{}, activitiesLimit: limit, activitiesIDGenerator: IDGenerator}
+	pool := Pool{activities: make(map[uint64]*Activity, limit), activitiesLimit: limit, activitiesIDGenerator: IDGenerator}
 	return &pool
 }
 
 var ErrActivityPoolReachedLimit = errors.New("the capacity of activity pool has reached the upper limit")
 
-func (p *Pool) NewActivity(name string) (*Activity, error) {
+// NewActivity initializes an activity and add it to the pool.
+//
+// Returns the pointer to the just added activity if added successfully.
+//
+// If the activity ID already exists and replacement is not allowed, it will return a pointer to the corresponding
+// activity and an ErrActivityExisted exception.
+//
+// If the upper limit is not reached, otherwise an ErrActivityPoolReachedLimit is returned.
+func (p *Pool) NewActivity(name string, replace bool) (*Activity, error) {
 	p.activitiesRWMutex.Lock()
 	defer p.activitiesRWMutex.Unlock()
+	id := p.GetActivityIDGenerator()()
+	if activity, existed := p.activities[id]; existed && !replace {
+		return activity, ErrActivityExisted
+	}
 	if uint32(len(p.activities)) >= p.activitiesLimit {
 		return nil, ErrActivityPoolReachedLimit
 	}
-	activity := Activity{p.GetActivityIDGenerator()(), name}
+	activity := Activity{id, name}
 	p.activities[activity.ID] = &activity
 	return &activity, nil
 }
@@ -39,12 +61,17 @@ func (p *Pool) GetActivityIDGenerator() func() uint64 {
 	return fn
 }
 
+// NewActivityID generates an ID with the timestamp of calling this method.
 func (p *Pool) NewActivityID() uint64 {
 	return uint64(time.Now().Unix())
 }
 
+var ErrActivityExisted = errors.New("activity existed")
 var ErrActivityNonExists = errors.New("activity not exists")
 
+// RemoveActivity removes the activity
+//
+// Returns nil if the activity exists and is successfully deleted, otherwise an ErrActivityNonExists is returned.
 func (p *Pool) RemoveActivity(id uint64) error {
 	p.activitiesRWMutex.Lock()
 	defer p.activitiesRWMutex.Unlock()
@@ -55,6 +82,9 @@ func (p *Pool) RemoveActivity(id uint64) error {
 	return ErrActivityNonExists
 }
 
+// GetActivity gets the activity.
+//
+// If the activity does not exist, an ErrActivityNonExists is returned.
 func (p *Pool) GetActivity(id uint64) (*Activity, error) {
 	p.activitiesRWMutex.RLock()
 	defer p.activitiesRWMutex.RUnlock()
--- models/activity/pool_test.go
+++ models/activity/pool_test.go
@@ -1,6 +1,9 @@
 package activity
 
 import (
+	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -9,8 +12,8 @@ import (
 
 var poolDefault *Pool
 
-func setupPool(t *testing.T) {
-	poolDefault = NewActivityPool(1, nil)
+func setupPool(t *testing.T, limit uint32) {
+	poolDefault = NewActivityPool(limit, nil)
 }
 
 func teardownPool(t *testing.T) {
@@ -18,39 +21,39 @@ func teardownPool(t *testing.T) {
 }
 
 func TestNewActivityPool(t *testing.T) {
-	t.Run("initiate an activity pool with limit 1 and default ID generator", func(t *testing.T) {
+	t.Run("initializes an activity pool with limit 1 and default ID generator", func(t *testing.T) {
 		pool := NewActivityPool(1, nil)
 		assert.NotNil(t, pool)
 		assert.Empty(t, pool.activities)
 
-		activity, err := pool.NewActivity("test-activity-1")
+		activity, err := pool.NewActivity("test-activity-1", true)
 		assert.NotNil(t, activity)
 		assert.Equal(t, uint64(time.Now().Unix()), activity.ID)
 		assert.Equal(t, "test-activity-1", activity.Name)
 		assert.Nil(t, err)
 		assert.NotEmpty(t, pool.activities)
 
-		activity, err = pool.NewActivity("test-activity-2")
+		activity, err = pool.NewActivity("test-activity-2", true)
 		assert.ErrorIs(t, err, ErrActivityPoolReachedLimit)
 		assert.Nil(t, activity)
 		assert.Len(t, pool.activities, 1)
 	})
 
-	t.Run("initiate an activity pool with limit 1 and user-defined ID generator", func(t *testing.T) {
+	t.Run("initializes an activity pool with limit 1 and user-defined ID generator", func(t *testing.T) {
 		pool := NewActivityPool(1, func() uint64 {
 			return uint64(time.Now().Unix()) + 1
 		})
 		assert.NotNil(t, pool)
 		assert.Empty(t, pool.activities)
 
-		activity, err := pool.NewActivity("test-activity-3")
+		activity, err := pool.NewActivity("test-activity-3", true)
 		assert.NotNil(t, activity)
 		assert.Equal(t, uint64(time.Now().Unix())+1, activity.ID)
 		assert.Equal(t, "test-activity-3", activity.Name)
 		assert.Nil(t, err)
 		assert.NotEmpty(t, pool.activities)
 
-		activity, err = pool.NewActivity("test-activity-4")
+		activity, err = pool.NewActivity("test-activity-4", true)
 		assert.ErrorIs(t, err, ErrActivityPoolReachedLimit)
 		assert.Nil(t, activity)
 		assert.Len(t, pool.activities, 1)
@@ -58,9 +61,9 @@ func TestNewActivityPool(t *testing.T) {
 }
 
 func TestPool_GetActivity(t *testing.T) {
-	setupPool(t)
+	setupPool(t, 1)
 	defer teardownPool(t)
-	activity, err := poolDefault.NewActivity("test-activity-5")
+	activity, err := poolDefault.NewActivity("test-activity-5", false)
 	assert.Equal(t, uint64(time.Now().Unix()), activity.ID)
 	assert.Equal(t, "test-activity-5", activity.Name)
 	assert.Nil(t, err)
@@ -79,9 +82,9 @@ func TestPool_GetActivity(t *testing.T) {
 }
 
 func TestPool_RemoveActivity(t *testing.T) {
-	setupPool(t)
+	setupPool(t, 1)
 	defer teardownPool(t)
-	activity, err := poolDefault.NewActivity("test-activity-5")
+	activity, err := poolDefault.NewActivity("test-activity-5", false)
 	assert.Equal(t, uint64(time.Now().Unix()), activity.ID)
 	assert.Equal(t, "test-activity-5", activity.Name)
 	assert.Nil(t, err)
@@ -97,5 +100,69 @@ func TestPool_RemoveActivity(t *testing.T) {
 		err := poolDefault.RemoveActivity(activity.ID)
 		assert.Nil(t, err)
 		assert.Len(t, poolDefault.activities, 0)
+	})
+}
+
+func TestPool_NewActivity(t *testing.T) {
+	const N = 5
+	setupPool(t, N)
+	defer teardownPool(t)
+
+	t.Run("in parallel", func(t *testing.T) {
+		poolDefault.activitiesLimit = N
+		index := uint64(10)
+		poolDefault.activitiesIDGenerator = func() uint64 {
+			return atomic.AddUint64(&index, 1)
+		}
+		var wgNotification, wgReady sync.WaitGroup
+		wgNotification.Add(1)
+		wgReady.Add(N)
+		for i := 0; i < N; i++ {
+			i := i
+			go func() {
+				wgNotification.Wait()
+				activity, err := poolDefault.NewActivity(fmt.Sprintf("%s-%d", "test-activity-6", i), false)
+				t.Log(activity)
+				assert.NotNil(t, activity)
+				assert.Nil(t, err)
+				wgReady.Done()
+			}()
+		}
+		wgNotification.Done()
+		t.Log("starting...")
+		wgReady.Wait()
+		t.Log("finished.")
+		assert.Len(t, poolDefault.activities, N)
+	})
+}
+
+func TestPool_NewActivity2(t *testing.T) {
+	const N = 5
+	setupPool(t, N)
+	defer teardownPool(t)
+
+	t.Run("replace existed", func(t *testing.T) {
+		poolDefault.activitiesIDGenerator = func() uint64 {
+			return 1
+		}
+		assert.Empty(t, poolDefault.activities)
+		activity, err := poolDefault.NewActivity("test-activity-7-1", false)
+		assert.NotNil(t, activity)
+		assert.Equal(t, activity.ID, uint64(1))
+		assert.Equal(t, activity.Name, "test-activity-7-1")
+		assert.Nil(t, err)
+		assert.Len(t, poolDefault.activities, 1)
+		activity, err = poolDefault.NewActivity("test-activity-7-2", false)
+		assert.NotNil(t, activity)
+		assert.Equal(t, activity.ID, uint64(1))
+		assert.Equal(t, activity.Name, "test-activity-7-1")
+		assert.ErrorIs(t, err, ErrActivityExisted)
+		assert.Len(t, poolDefault.activities, 1)
+		activity, err = poolDefault.NewActivity("test-activity-7-3", true)
+		assert.NotNil(t, activity)
+		assert.Equal(t, activity.ID, uint64(1))
+		assert.Equal(t, activity.Name, "test-activity-7-3")
+		assert.Nil(t, err)
+		assert.Len(t, poolDefault.activities, 1)
 	})
 }
